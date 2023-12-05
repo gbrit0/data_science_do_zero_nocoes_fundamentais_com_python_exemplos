b229864a06ab04c29125fa8482f60be78e7722f8
Gabriel Brito Ribeiro <gbrito341@gmail.com>
2023-12-05 16:11:00 -0300

    Cientistas de dados que talvez você conheça

--- cap01.py
+++ cap01.py
@@ -51,6 +51,7 @@ num_friends_by_id.sort(key=lambda id_and_friends: id_and_friends[1], # ordena a 
 from collections import Counter # não é carregado por padrão
 
 def friends_of_friends(user): # contagem de amigos em comum do user. Retorna o id do não amigo e a contagem de conexões em comum
+        # """foaf significa 'friend of a friend' [amigo de um amigo]"""
     user_id = user["id"]
     return Counter(
         foaf_id
@@ -60,4 +61,56 @@ def friends_of_friends(user): # contagem de amigos em comum do user. Retorna o i
         and foaf_id not in friendships[user_id]  # e não sejam meus amigos
     )
 
-print(friends_of_friends(users[3])) # Counter({0: 2, 5: 1})
+# print(friends_of_friends(users[3])) # Counter({0: 2, 5: 1})
+
+#CIENTISTAS DE DADOS QUE TALVEZ VOCÊ CONHEÇA:
+
+interests = [
+    (0,"Hadoop"),(0,"Big Data"),(0,"HBase"),(0,"Java"),
+    (0,"Spark"),(0,"Storm"),(0,"Cassandra"),
+    (1,"NoSQL"),(1,"MongoDB"),(1,"Cassandra"),(1,"HBase"),
+    (1, "Postgres"),(2,"Python"),(2,"scikit-learn"),(2,"scipy"),
+    (2,"numpy"),(2,"statsmodels"),(2,"pandas"),(3,"R"),(3,"Python"),
+    (3,"statistics"),(3,"regression"),(3,"probability"),
+    (4,"machine learning"),(4,"regression"),(4,"decision trees"),
+    (4,"libsvm"),(5,"Python"),(5,"R"),(5,"Java"),(5,"C++"),
+    (5,"Haskell"),(5,"programming languages"),(6,"statistics"),
+    (6,"probability"),(6,"mathematics"),(6,"theory"),
+    (7,"machine learning"),(7,"scikit-learn"),(7,"Mahout"),
+    (7,"neural networks"),(8,"neural networks"),(8,"deep learning"),
+    (8,"Big Data"),(8,"artificial intelligence"),(9,"Hadoop"),
+    (9,"Java"),(9,"MapReduce"),(9,"Big Data")
+]
+
+# construir uma função para encontrar usuários com o mesmo interesse
+def data_scientists_who_like(target_interest):
+    """Encontre os ids dos usuários com o mesmo interesse"""
+    return [user_id
+    for user_id, user_interest in interests
+    if user_interest == target_interest]
+# funciona mas tem que examinar a lista a cada busca
+
+from collections import defaultdict
+
+# As chaves são interesses, os valores são listas de user_ids com o interesse em questão
+user_ids_by_interest = defaultdict(list) # função usada pra criar valores dict a partir de uma lista
+
+for user_id, interest in interests:
+    user_ids_by_interest[interest].append(user_id)
+
+
+# As chaves são user_ids, os valores são listas de interesses do user_ids em questão
+interests_by_user_id = defaultdict(list) # função usada pra criar valores dict a partir de uma lista
+
+for user_id, interest in interests:
+    interests_by_user_id[user_id].append(interest)
+
+def most_common_interests_with(user):
+    return Counter(
+        interested_user_id
+        for interest in interests_by_user_id[user["id"]]
+        for interested_user_id in user_ids_by_interest[interest]
+        if interested_user_id != user["id"]
+    )
+
+# print(most_common_interests_with(users[3]))
